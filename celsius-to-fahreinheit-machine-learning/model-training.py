5426f717df494fe939fe4690bbcf7cbf8023e1fd
Felipe Henriques <137960457+Felipehnasci@users.noreply.github.com>
2025-03-27 21:22:24 -0300

    Update model-training.py

--- celsius-to-fahreinheit-machine-learning/model-training.py
+++ celsius-to-fahreinheit-machine-learning/model-training.py
@@ -2,25 +2,22 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
-# Carregar os dados
-temperature_df = pd.read_csv(r'D:\Projetos\Python\Aprendendo Machine Learning\celsius-to-fahreinheit-machine-learning\Celsius-to-Fahrenheit.csv')
 
-# Definir os dados de treino
+# MUDE PARA O DIRETORIO DO SEU ARQUIVO "Celsius-to-Fahrenheit.csv"
+# CHANGE THE DIRECTORY OF YOUR "Celsius-to-Fahrenheit.csv" ARCHIVE
+temperature_df = pd.read_csv(r'D:Your directory here\Celsius-to-Fahrenheit.csv')
+
 x_train = temperature_df['Celsius'].values.reshape(-1, 1)
 y_train = temperature_df['Fahrenheit'].values.reshape(-1, 1)
 
-# Criar o modelo
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(units=1, input_shape=[1])
 ])
 
-# Compilar o modelo
 model.compile(optimizer=tf.keras.optimizers.Adam(0.5), loss='mean_squared_error')
 
-# Treinar o modelo
 model.fit(x_train, y_train, epochs=300, verbose=1)
 
-# Salvar o modelo treinado
 model.save('model_celsius_to_fahrenheit.h5')
 
-print("Modelo treinado e salvo com sucesso!")
+print("Modelo treinado e salvo com sucesso! // Model has been succesfuly trained !")
